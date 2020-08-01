fe23eec49b5c44cc6e3c6ba38fb7fc372dd841fd
Bill Petridis <bpetridis1@gmail.com>
2020-08-01 20:54:41 +1000

    Added Phantasma Energy contract - Compiling but not deploying
    Also added entry in the deploy file. It compiles fine in truffle, just wheen running a migrate or deploy the new contract doesn't deploy. No errors though.

---
+++ contracts/PhantasmaEnergy.sol
@@ -0,0 +1,264 @@
+/*
+Implements EIP20 token standard: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+.*/
+
+pragma solidity ^0.5.16;
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ *
+ * Arithmetic operations in Solidity wrap on overflow. This can easily result
+ * in bugs, because programmers usually assume that an overflow raises an
+ * error, which is the standard behavior in high level programming languages.
+ * `SafeMath` restores this intuition by reverting the transaction when an
+ * operation overflows.
+ *
+ * Using this library instead of the unchecked operations eliminates an entire
+ * class of bugs, so it's recommended to use it always.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts with custom message when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
+
+contract PhantasmaEnergy {
+
+	using SafeMath for uint256;
+
+    uint256 constant private MAX_UINT256 = 2**256 - 1;
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+	
+	uint256 private _totalSupply;
+    address private _producer;
+	
+    function name() public pure returns (string memory) {
+        return "Phantasma Energy";
+    }
+
+    function symbol() public pure returns (string memory) {
+        return "KCAL";
+    }
+	
+    function decimals() public pure returns (uint8) {
+        return 8;
+    }
+
+    constructor() public {
+        _totalSupply = 0;                        
+		_producer = address(0);
+    }
+	
+	function toString(address x) internal pure returns (string memory) {
+		bytes memory b = new bytes(20);
+		for (uint i = 0; i < 20; i++)
+			b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
+		return string(b);
+	}
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_balances[msg.sender] >= _value);
+        _balances[msg.sender] = _balances[msg.sender].sub(_value);
+        _balances[_to] = _balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        uint256 allowance = _allowances[_from][msg.sender];
+        require(_balances[_from] >= _value && allowance >= _value);
+        _balances[_to] = _balances[_to].add(_value);
+        _balances[_from] = _balances[_from].sub(_value);
+        if (allowance < MAX_UINT256) {
+            _allowances[_from][msg.sender] -= _value;
+        }
+        emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        _allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return _allowances[_owner][_spender];
+    }
+	
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+	function swapInit(address target) public returns (bool) {
+		require(_producer == address(0));
+        require(target != address(0));
+		
+		_producer = target;
+		swapIn("debug", target, 10000000000000000); // deposit some SOUL into the initial address, for debug purposes
+		return true;
+	}
+
+    function swapIn(string memory source, address target, uint256 amount) public returns (bool success) {
+        require(_producer != address(0));
+		require(msg.sender == _producer);
+        _totalSupply = _totalSupply.add(amount);
+        _balances[target] = _balances[target].add(amount);
+		emit Transfer(address(0), target, amount);
+        emit Swap(source, toString(target), amount);
+		return true;
+    }
+
+    function swapOut(address source, string memory target, uint256 amount) public returns (bool success) {
+		require(msg.sender == _producer);
+		require(_balances[source] >= amount);
+		require(_totalSupply >= amount);
+		
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[source] = _balances[source].sub(amount);
+		emit Transfer(source, address(0), amount);
+        emit Swap(toString(source), target, amount);
+		return true;
+    }
+	
+    // solhint-disable-next-line no-simple-event-func-name
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);	
+    event Swap(string _from, string _to, uint256 _value);
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 var Phantasma = artifacts.require("Phantasma");
 module.exports = function(deployer) {
     deployer.deploy(Phantasma);
+	deployer.deploy(PhantasmaEnergy);
     // Additional contracts can be deployed here
 };
